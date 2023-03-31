b2d6ad491e6f24887f2db06ba9d7a65ef5040fad
Lucas Vocanson <lvocanson@gaming.tech>
2023-03-31 20:21:59 +0200

    Save first version

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public GameData Data { get; private set; }
 
     private void Awake()
     {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
+        if (Instance != null)
         {
             Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        Data = Save.LoadFile("save.dat");
+        if (Data == null)
+        {
+            Data = new GameData();
         }
     }
 
