d8a356c5e7274bd04e8bc45ec10a1b463135965b
Lucas Vocanson <lvocanson@gaming.tech>
2023-03-31 19:34:53 +0200

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
