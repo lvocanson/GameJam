7c9d377773094dc69025b5a6b0805a378c547e85
Cyriansavary <92294053+Cyriansavary@users.noreply.github.com>
2023-04-02 01:58:32 +0200

    Update BackgroundManager.cs

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
+    [SerializeField] GameObject matin;
+    [SerializeField] GameObject jour;
+    [SerializeField] GameObject soire;
+    [SerializeField] GameObject nuit;
     private void Update()
     {
+        switch (GameManager.Instance.Data.TimeDay)
+        {
+            case 0:
+                matin.SetActive(true);
+                jour.SetActive(false);
+                soire.SetActive(false);  
+                nuit.SetActive(false);
+                break;
+            case 1:
+                matin.SetActive(false);
+                jour.SetActive(true);
+                soire.SetActive(false);
+                nuit.SetActive(false);
+                break;
+            case 2:
+                matin.SetActive(false);
+                jour.SetActive(false);
+                soire.SetActive(true);
+                nuit.SetActive(false);
+                break;
+            case 3:
+                matin.SetActive(false);
+                jour.SetActive(false);
+                soire.SetActive(false);
+                nuit.SetActive(true);
+                break;
+            default:
+                break;
+        }
 
-        transform.GetChild(GameManager.Instance.Data.TimeDay).gameObject.SetActive(true);
-        
-       
+
+
     }
 
 
