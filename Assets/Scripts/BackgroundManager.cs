0b48b635628d0b9ca7334d0ede248d957d905e43
Cyriansavary <92294053+Cyriansavary@users.noreply.github.com>
2023-04-01 22:41:28 +0200

    lil fix

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Update()
+    {
+                
+    }
+
+
+}
