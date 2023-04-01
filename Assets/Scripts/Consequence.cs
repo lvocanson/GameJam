8bd956f9841a1bf208331795a98ab989b69660b5
Lucas Vocanson <lvocanson@gaming.tech>
2023-04-01 15:37:55 +0200

    Fork in GameManager

--- Assets/Scripts/Consequence.cs
+++ Assets/Scripts/Consequence.cs
@@ -1,7 +1,12 @@
 public struct IntFork
 {
-    public int MinValue,
-               MaxValue;
+    public IntFork(int min, int max)
+    {
+        Min = min;
+        Max = max;
+    }
+    public int Min,
+               Max;
 }
 
 public class Consequence
@@ -24,16 +29,16 @@ public class Consequence
     public void ApplyConsequence()
     {
         GameData data = GameManager.Instance.Data;
-        data.treasury += UnityEngine.Random.Range(treasury.MinValue, treasury.MaxValue);
-        data.landOwned += UnityEngine.Random.Range(landOwned.MinValue, landOwned.MaxValue);
-        data.population += UnityEngine.Random.Range(population.MinValue, population.MaxValue);
+        data.treasury += UnityEngine.Random.Range(treasury.Min, treasury.Max);
+        data.landOwned += UnityEngine.Random.Range(landOwned.Min, landOwned.Max);
+        data.population += UnityEngine.Random.Range(population.Min, population.Max);
         for (int i = 0; i < (int)SocialClass.Length; i++)
         {
-            data.friendshipScores[i] += UnityEngine.Random.Range(friendshipScores[i].MinValue, friendshipScores[i].MaxValue);
+            data.friendshipScores[i] += UnityEngine.Random.Range(friendshipScores[i].Min, friendshipScores[i].Max);
         }
         for (int i = 0; i < (int)Country.Length; i++)
         {
-            data.diplomacyScores[i] += UnityEngine.Random.Range(diplomacyScores[i].MinValue, diplomacyScores[i].MaxValue);
+            data.diplomacyScores[i] += UnityEngine.Random.Range(diplomacyScores[i].Min, diplomacyScores[i].Max);
         }
 
     }               
