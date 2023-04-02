81ff8c54b39474485a7b28e8b491fb6925f92dfb
Lucas Vocanson <lvocanson@gaming.tech>
2023-04-02 03:37:22 +0200

    Removed unnecessary things

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.U2D;
 using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
@@ -40,10 +37,5 @@ public class BackgroundManager : MonoBehaviour
             default:
                 break;
         }
-
-
-
     }
-
-
 }
